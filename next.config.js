ee96c0adef0186a958ab9a1840d5e230ed414d9c
betchabippy <tullie@betchabippy.com>
2026-05-22 19:00:37 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  turbopack: {},
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: '**.supabase.co' },
       { protocol: 'https', hostname: 'images.unsplash.com' },
     ],
-  },
-  webpack: (config) => {
-    config.resolve.alias = { ...config.resolve.alias, 'mapbox-gl': 'mapbox-gl' }
-    return config
   },
 }
 
